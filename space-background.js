70f22d4f17f1c4d3aef6ef9bd09a90c94389e5fc
Sashank999 <bandi.rao999@gmail.com>
2023-12-14 15:32:28 +0530

    Change background to reuse generated nodes.

--- space-background.js
+++ space-background.js
@@ -5,25 +5,22 @@ const windowWidth = window.innerWidth;
 const windowHeight = window.innerHeight;
 
 if (background) {
-	showDots();
-	setInterval(showDots, 1000);
-}
-
-function showDots() {
-	// Remove all current dots
-	background.textContent = "";
-
-	// Add new dots
-	const nodes = [];
 	let newDot;
 	for (let i = 0; i < NUMBER_OF_DOTS; i++) {
 		newDot = document.createElement("div");
 		newDot.className = "space-dot";
-		newDot.style = `top: ${randomBetween(1, windowHeight)}px; left: ${randomBetween(1, windowWidth)}px;`;
 		background.appendChild(newDot);
-		nodes.push(newDot);
 	}
-	setTimeout(() => nodes.forEach(x => x.remove()), 2000);
+
+	replaceDots();
+	setInterval(replaceDots, 1000);
+}
+
+function replaceDots() {
+	// Change position of dots
+	[...background.children].forEach((dot) => {
+		dot.style = `top: ${randomBetween(1, windowHeight)}px; left: ${randomBetween(1, windowWidth - 10)}px;`;
+	});
 }
 
 function randomBetween(min, max) {
