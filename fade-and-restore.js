0195359a97d22f65a8d90dc69ffc90cb54b5f7ed
Sashank999 <bandi.rao999@gmail.com>
2023-12-15 13:53:42 +0530

    Add text fading and reappearing animation.

---
+++ fade-and-restore.js
@@ -0,0 +1,24 @@
+const ALL_SECTIONS = ["#start", "#qualifications", "#previous-projects", "#contact", "#thank-you"];
+const TOTAL_SECTIONS = ALL_SECTIONS.length;
+
+let count = 0;
+
+const observer = new IntersectionObserver((entries, observer) => {
+	entries.forEach((entry) => {
+		if (entry.target.className.includes("visible")) return;
+
+		if (entry.intersectionRatio >= 0.25) {
+			entry.target.classList.add("visible");
+			count++;
+		}
+
+		if (count === TOTAL_SECTIONS) observer.disconnect();
+	})
+}, {
+	root: null,
+	threshold: 0.25
+});
+
+ALL_SECTIONS.forEach((selector) => {
+	observer.observe(document.querySelector(selector));
+});
