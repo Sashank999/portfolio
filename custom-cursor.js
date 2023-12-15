ffded21b351572b99b1661f07c7c7d336f96d09b
Sashank999 <bandi.rao999@gmail.com>
2023-12-15 15:27:30 +0530

    Remmove custom cursor.

--- custom-cursor.js
+++
@@ -1,7 +0,0 @@
-const cursor = document.querySelector("#cursor");
-document.addEventListener("mousemove", (e) => {
-	cursor.style.top = e.clientY - 10 + "px";
-	cursor.style.left = e.clientX - 10 + "px";
-});
-
-document.body.style.cursor = "none";
