095bb8213b1afe89f4c191e354ab31ffec9d4c84
Sashank999 <bandi.rao999@gmail.com>
2023-12-26 15:44:45 +0530

    Add Prettier configuration.

--- fade-and-restore.js
+++ fade-and-restore.js
@@ -3,21 +3,24 @@ const TOTAL_SECTIONS = ALL_SECTIONS.length;
 
 let count = 0;
 
-const observer = new IntersectionObserver((entries, observer) => {
-	entries.forEach((entry) => {
-		if (entry.target.className.includes("visible")) return;
+const observer = new IntersectionObserver(
+	(entries, observer) => {
+		entries.forEach((entry) => {
+			if (entry.target.className.includes("visible")) return;
 
-		if (entry.intersectionRatio >= 0.25) {
-			entry.target.classList.add("visible");
-			count++;
-		}
+			if (entry.intersectionRatio >= 0.25) {
+				entry.target.classList.add("visible");
+				count++;
+			}
 
-		if (count === TOTAL_SECTIONS) observer.disconnect();
-	})
-}, {
-	root: null,
-	threshold: 0.25
-});
+			if (count === TOTAL_SECTIONS) observer.disconnect();
+		});
+	},
+	{
+		root: null,
+		threshold: 0.25,
+	}
+);
 
 ALL_SECTIONS.forEach((selector) => {
 	observer.observe(document.querySelector(selector));
--- quotes.js
+++ quotes.js
@@ -1,15 +1,15 @@
 // Fetches a quote and displays it in the first section
 
 fetch("https://api.quotable.io/quotes/random?maxLength=90")
-.then((data) => data.json())
-.then((json) => {
-	const quote = json[0].content;
-	const author = json[0].author;
+	.then((data) => data.json())
+	.then((json) => {
+		const quote = json[0].content;
+		const author = json[0].author;
 
-	const quoteContainer = document.querySelector(".quote");
-	quoteContainer.children[0].textContent = quote;
-	quoteContainer.children[2].textContent = "- " + author;
-})
-.catch((error) => {
-	console.error(error);
-});
+		const quoteContainer = document.querySelector(".quote");
+		quoteContainer.children[0].textContent = quote;
+		quoteContainer.children[2].textContent = "- " + author;
+	})
+	.catch((error) => {
+		console.error(error);
+	});
