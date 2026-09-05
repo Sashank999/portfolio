fd4603b5270e1004afe1df8d03212b9506304e1c
Sashank999 <bandi.rao999@gmail.com>
2026-09-05 17:28:31 +0530

    Add diffcsv repo to repo cards.

--- repo-cards.js
+++ repo-cards.js
@@ -1,6 +1,6 @@
 // @ts-check
 
-const REPO_NAMES = ["Sashank999/spacex-clone", "Sashank999/static-qr-gen", "Sashank999/static-qr-gen-react"];
+const REPO_NAMES = ["Sashank999/spacex-clone", "Sashank999/static-qr-gen", "Sashank999/static-qr-gen-react", "Sashank999/diffcsv"];
 const DEFAULT_CACHE_TIMEOUT = 5 * 60 * 60;
 
 // The below SVG is copied from GitHub.
@@ -11,6 +11,7 @@ const GITHUB_LANGUAGE_COLORS = {
 	CSS: "#663399",
 	JavaScript: "#f1e05a",
 	TypeScript: "#3178c6",
+	Rust: "#b7410e",
 };
 const GITHUB_STAR_SVG = `<svg height="16" viewBox="0 0 16 16" width="16">
     <path d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Zm0 2.445L6.615 5.5a.75.75 0 0 1-.564.41l-3.097.45 2.24 2.184a.75.75 0 0 1 .216.664l-.528 3.084 2.769-1.456a.75.75 0 0 1 .698 0l2.77 1.456-.53-3.084a.75.75 0 0 1 .216-.664l2.24-2.183-3.096-.45a.75.75 0 0 1-.564-.41L8 2.694Z"></path>
